bf75a12099271960de9b76e05579fd32d31e912d
Emsi App <emsiapp@Jonathans-MacBook-Pro.local>
2017-10-22 17:00:11 -0600

    fixed warning

--- src/main/java/com/jallen/model/CMAppException.java
+++ src/main/java/com/jallen/model/CMAppException.java
@@ -1,6 +1,7 @@
 package com.jallen.model;
 
 public class CMAppException extends Exception {
+	private static final long serialVersionUID = -5595815051892161207L;
 
 	public CMAppException(String message) {
 		super(message);
